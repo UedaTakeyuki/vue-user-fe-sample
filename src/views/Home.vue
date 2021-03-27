8283ca50803fbcecc7639a0032fbfaee42cccfcf
Takeyuki UEDA <ueda@latelierdueda.com>
2021-03-27 19:37:14 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <VueUserFeSample />
+    <FccButton />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import * as vuf from "vue-user-fe"
+var VueUserFeSample = vuf.VueUserFeSample
+var FccButton = vuf.FccButton
+//var keroyon = vuf.kerokero
+//var VueUserFeSample = require("vue-user-fe").VueUserFeSample
+//var FccButton = require("vue-user-fe").FccButton
+//var keroyon = require("vue-user-fe").kerokero
+export default {
+  name: 'Home',
+  components: {
+    VueUserFeSample,
+    FccButton
+  },
+  mounted(){
+    vuf.kerokero()
+//    console.log(vuf)
+  }
+}
+</script>
